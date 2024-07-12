b0b9ae25f8e60972f76081b773f7805b8e956877
CodeDestroyer2806 <38956102+CodeDestroyer2806@users.noreply.github.com>
2024-07-12 16:37:17 +0400

    Added Skelton Loading Effect

--- app/_components/Listing.jsx
+++ app/_components/Listing.jsx
@@ -6,16 +6,14 @@ function Listing({listing}) {
   return (
     <div>
         <div className='grid grid-cols-1 md:grid grid-cols-2 gap-5'>
-            {listing.map((item, index) => (
+            {listing?.length>0? listing.map((item, index) => (
                 <div className='p-3 hover:border hover:border-primary cursor-pointer rounded-lg'>
                     <Image src={item.listingImages[0].url} width={800} height={150}
                     className="rounded-lg object-cover h-[170px]" alt=""/>
                     <div className='flex mt-2 flex-col gap-2'>
                         <h2 className='font-bold text-xl'>${item.price}</h2>
                         <h2 className='flex gap-2 text-sm text-gray-400'><MapPin className='h-5 w-5'/>{item.address}</h2>
-                    </div>
-                <div className='flex gap-2 mt-2 justify-between'>
-                    <h2 className='flex gap-2 w-full text-sm bg-slate-100 rounded-md p-2 text-gray-500 justify-center items-center'>
+                        <h2 className='flex gap-2 w-full text-sm bg-slate-100 rounded-md p-2 text-gray-500 justify-center items-center'>
                         <BedDouble className='h-5 w-5'/>
                         {item?.bedroom}
                     </h2>
@@ -29,10 +27,16 @@ function Listing({listing}) {
                         <Ruler className='h-5 w-5'/>
                         {item?.area}
                     </h2>
-                </div>
+                    </div>
 
                 </div>
-            ))}
+            ))
+        :[1,2,3,4,5,6,7,8].map((item, index) => (
+            <div key={index} className='h-[230px] w-full bg-slate-200 animate-pulse'>
+
+            </div>
+        ))
+        }
         </div>
     </div>
   )
